47ec5e6caeb821474b7768de1af3040a5f768feb
Dariusz Cybulski <d.cybulski95@gmail.com>
2023-12-18 19:44:16 +0100

    feat: added signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
   @Post('login')
   login() {
     return this.authService.login();
-  }
-
-  @Post('signup')
-  signup() {
-    return this.authService.signup();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { DbService } from '../db/db.service';
+import { AuthDto } from './dto/auth.dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private dbService: DbService) {}
 
-  login() {
-    return 'i am login';
+  async signup(dto: AuthDto) {
+    const passwordHash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.dbService.user.create({
+        data: {
+          email: dto.email,
+          hash: passwordHash,
+        },
+      });
+
+      const { hash: _, ...returnUser } = user;
+      return returnUser;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('User already exists');
+        }
+      }
+    }
   }
 
-  signup() {
-    return 'i am signup';
+  login() {
+    return 'i am login';
   }
 }
