120a27ff277f1f34a501dc0d64b7bdf8c4668aa8
Dariusz Cybulski <d.cybulski95@gmail.com>
2023-12-21 14:02:24 +0100

    feat: added ingredients module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/db.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, DbModule],
+  imports: [AuthModule, UsersModule, DbModule, IngredientsModule],
   controllers: [],
   providers: [],
 })
